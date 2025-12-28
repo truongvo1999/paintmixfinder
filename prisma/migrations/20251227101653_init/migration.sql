60d7b2d45fdf6043f10089ccbd4587246903acbd
truongvo1999 <chitruong.vo@gmail.com>
2025-12-28 12:25:45 +0700

    Fix migration timestamp types for Postgres

--- prisma/migrations/20251227101653_init/migration.sql
+++ prisma/migrations/20251227101653_init/migration.sql
@@ -4,9 +4,8 @@ CREATE TABLE "new_ImportState" (
     "brandsDone" BOOLEAN NOT NULL DEFAULT false,
     "colorsDone" BOOLEAN NOT NULL DEFAULT false,
     "componentsDone" BOOLEAN NOT NULL DEFAULT false,
-    "updatedAt" DATETIME NOT NULL
+    "updatedAt" TIMESTAMP(3) NOT NULL
 );
 INSERT INTO "new_ImportState" ("brandsDone", "colorsDone", "componentsDone", "id", "updatedAt") SELECT "brandsDone", "colorsDone", "componentsDone", "id", "updatedAt" FROM "ImportState";
 DROP TABLE "ImportState";
 ALTER TABLE "new_ImportState" RENAME TO "ImportState";
-
